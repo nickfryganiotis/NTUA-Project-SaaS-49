a94fc67660001e2e47209d7d928dcdca762df1e3
el16104 <johnpapageorgiou0@gmail.com>
2021-07-03 12:02:09 +0300

    Added question, answer, homepage comps

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import SignIn from './SignIn'
 import SignUp from './SignUp';
+import QuestionForm from './QuestionForm'
 import './App.css';
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
             <Switch>
             <Route path="/" exact component = {SignIn} />
             <Route path="/sign_up" exact component = {SignUp} />
+            <Route path="/ask" exact component={QuestionForm} />
+            <Route path="/answer" exact component={AnswerForm} />
             </Switch>
         </div>
       </container>
