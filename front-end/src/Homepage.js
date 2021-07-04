30522f998768108efe786014cd33a300c411dad6
el16104 <johnpapageorgiou0@gmail.com>
2021-07-04 20:43:14 +0300

    MODIFIED FRONTEND

---
+++ front-end/src/Homepage.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Homepage(){
+    return(
+        <div>
+            <h2>Welcome to AskMeAnything</h2>
+            <table>
+                <tbody>
+                <tr>
+                    <td>
+                        <button>Ask a question</button>
+                        <button>Answer a question</button>
+                    </td>
+                    <td>
+                        PLACEHOLDER FOR GRAPH
+                    </td>
+                    <td>
+                        PLACEHOLDER FOR GRAPH
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
